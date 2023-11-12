1c73be02869d67411f0932767b7f5f168fc2be48
SoImKiyo <85789841+SoimKiyo@users.noreply.github.com>
2023-11-12 18:35:13 +0100

    Obligation de remplir les champs sur le QCM

--- pages/script/script-finaltest.js
+++ pages/script/script-finaltest.js
@@ -1,54 +1,44 @@
 function verifierReponses() {
-    var questions = document.querySelectorAll('.question');
-    var firstName = document.getElementById('firstName').value;
-    var lastName = document.getElementById('lastName').value;
-    var score = 0;
-    var totalQuestions = questions.length;
-  
-    for (var i = 0; i < questions.length; i++) {
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+
+  // Vérifie si les champs de prénom et de nom sont remplis
+  if (firstName.trim() === '' || lastName.trim() === '') {
+      alert('Veuillez remplir tous les champs avant de vérifier.');
+      return;
+  }
+
+  var questions = document.querySelectorAll('.question');
+  var score = 0;
+  var totalQuestions = questions.length;
+
+  for (var i = 0; i < questions.length; i++) {
       var checkboxes = questions[i].querySelectorAll('input[type="checkbox"]');
       var userAnswer = null;
-  
+
       for (var j = 0; j < checkboxes.length; j++) {
-        if (checkboxes[j].checked) {
-          userAnswer = checkboxes[j].value === 'true';
-  
-          if (userAnswer) {
-            checkboxes[j].parentNode.style.backgroundColor = '#4caf50'; // Vert
-            score++;
-          } else {
-            checkboxes[j].parentNode.style.backgroundColor = '#e57373'; // Rouge
+          if (checkboxes[j].checked) {
+              userAnswer = checkboxes[j].value === 'true';
+
+              if (userAnswer) {
+                  checkboxes[j].parentNode.style.backgroundColor = '#4caf50'; // Vert
+                  score++;
+              } else {
+                  checkboxes[j].parentNode.style.backgroundColor = '#e57373'; // Rouge
+              }
+
+              // Désactive l'autre case
+              checkboxes.forEach(function (checkbox, index) {
+                  if (index !== j) {
+                      checkbox.disabled = true;
+                  }
+              });
+
+              break;
           }
-  
-          // Désactive l'autre case de la même question
-          checkboxes.forEach(function (otherCheckbox, index) {
-            if (index !== j) {
-              otherCheckbox.disabled = true;
-            }
-          });
-  
-          break;
-        }
       }
-    }
-  
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `${firstName} ${lastName}, Votre score : ${score}/${totalQuestions} <br> Projet pour la NSI Site de E-Learning`;
   }
-  
-  // Ajout d'un gestionnaire d'événements pour chaque paire de cases de la même question
-  var questions = document.querySelectorAll('.question');
-  questions.forEach(function (question) {
-    var checkboxes = question.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function (checkbox) {
-      checkbox.addEventListener('change', function () {
-        // Désactive toutes les autres cases de la même question
-        checkboxes.forEach(function (otherCheckbox) {
-          if (otherCheckbox !== checkbox) {
-            otherCheckbox.disabled = checkbox.checked;
-          }
-        });
-      });
-    });
-  });
-  
+
+  var resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = `${firstName} ${lastName}, Votre score : ${score}/${totalQuestions} <br> Projet pour la NSI Site de E-Learning`;
+}
